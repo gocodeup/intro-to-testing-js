2b04cfcb70d9ff3d0ebcf396e1c84f7b4bc22ac7
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-10-11 15:04:46 -0500

    Second Exercise from Testing

--- code.js
+++ code.js
@@ -4,5 +4,5 @@ function helloWorld(){
 }
 
 function sayHello(){
-
+    return "";
 }
--- tests.js
+++ tests.js
@@ -18,4 +18,8 @@ describe('sayHello',function () {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
+    });
+
 });
