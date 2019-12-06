bfbdbe917d5670a36b07dc61b1a111584cad81d7
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2019-12-06 10:41:39 -0600

    finished first tiny test

--- code.js
+++ code.js
@@ -3,12 +3,25 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(){
+function sayHello(name){
 
+    return "Hello, " + name + "!"
 }
-// function sayHello(name){
-//         return "Hello " + name + "!"
+
+// function sayHello(){
+//     return "Hello, Jane!";
+// }
+
+// function sayHello(name) {
+//     if (name === "Alex") {
+//         return "Hello, Alex!";
+//     }else if(name === 'Pat') {
+//         return "Hello, Pat!";
+//
+//     } else{
+//         return "Hello, Jane!"
 //     }
+// }
 
 
 
--- tests.js
+++ tests.js
@@ -20,6 +20,18 @@ describe('sayHello', function() {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return the string "Hello, Pat!" when executed', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
 });
 
 
