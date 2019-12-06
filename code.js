aee0c1d9e8d2efb003752b0fd43f4002317711ca
Paris Tyus <Tyus.Paris1@gmail.com>
2019-12-06 10:21:25 -0600

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,18 @@
-// helloWorld function 
-function helloWorld() {
+var test = true;
+
+
+// helloWorld function
+ var helloWorld = function() {
     return "Hello, World!";
+}
+
+//exercise 02 -- 4 specs, 4 failures function not defined//
+//exercise 03 -- hello string did not return//
+//exercise 04 -- function stays green because its defined; all of test are logging//
+//exercise 05 -- complete
+//exercise 06 -- complete
+
+//exercise 1//
+function sayHello(){
+
 }
--- tests.js
+++ tests.js
@@ -1,15 +1,37 @@
-// Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+
+
+describe('sayHello', function () {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
     });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+    it('to be', function () {
+        expect(test).toBe(true);
     });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
-    });
-    it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+    it('not to be', function () {
+        expect(test).not.toBe(false);
     });
 });
+
+
+
+
+
+
+
+// // Unit tests for the helloWorld function
+// describe('helloWorld', function() {
+//     it('should be a defined function', function() {
+//       expect(typeof helloWorld).toBe('function');
+//     });
+//     it('should return a string when called', function() {
+//       expect(typeof helloWorld()).toBe("string");
+//     });
+//     it('should return the string "Hello, World!" when executed', function() {
+//       expect(helloWorld()).toBe("Hello, World!");
+//     });
+//     it("should never return 'undefined' when called", function() {
+//       expect(helloWorld()).not.toBe(undefined);
+//     });
+// });
+
+
