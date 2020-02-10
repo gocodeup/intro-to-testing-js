f1e3d73730de7ff984cc0c65ff27def36b0fc0fb
Casey Friday <casey@friday-next.com>
2020-02-10 14:29:34 -0600

    Added sayHello test for string

--- code.js
+++ code.js
@@ -5,5 +5,5 @@ function helloWorld() {
 
 // sayHello function
 function sayHello(name) {
-
+    return "";
 }
--- tests.js
+++ tests.js
@@ -19,4 +19,7 @@ describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
 });
