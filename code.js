28d24b8059abe8e5214b299d9829da10391ef121
jaya <jayaseyyadri@gmail.com>
2020-10-01 14:44:36 -0500

    committing jasmine test 2

--- code.js
+++ code.js
@@ -3,5 +3,5 @@ function helloWorld() {
     return "Hello, World!";
 }
 function sayHello(){
-
+    return "";
 }
--- tests.js
+++ tests.js
@@ -16,8 +16,11 @@ describe('helloWorld', function() {
 
 
 //Unit test for sayHello function
-describe('helloWorld', function() {
+describe('sayHello', function() {
     it('should be a defined function', function() {
-        expect(typeof helloWorld).toBe('function');
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function (){
+        expect(typeof sayHello()).toBe("string" );
     });
 });
