a4c0c467780b900a152737a88ba6beeb8005640f
jaya <jayaseyyadri@gmail.com>
2020-10-01 15:10:06 -0500

    committing jasmine test 4

--- code.js
+++ code.js
@@ -8,3 +8,9 @@ function sayHello(){
 function sayHello(){
     return 'Hello, Jane!';
 }
+function sayHello(input){
+    if( input==='Alex') {
+        return 'Hello, Alex!';
+    }else{
+        return 'Hello, Jane!';}
+}
--- tests.js
+++ tests.js
@@ -17,15 +17,17 @@ describe('helloWorld', function() {
 
 //Unit test for sayHello function
 describe('sayHello', function() {
-    it('should be a defined function', function() {
+    it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
-    it('should return a string when called', function (){
-        expect(typeof sayHello()).toBe("string" );
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
     });
-    it('should return the string "Hello, Jane!" when executed', function (){
-        expect( sayHello('Jane')).toBe("Hello, Jane!" );
+    it('should return the string "Hello, Jane!" when executed', function () {
+        expect(sayHello('Jane')).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!" when executed', function () {
+        expect(sayHello('Alex')).toBe("Hello, Alex!");
     });
 });
-
 //Unit test for sayHello
