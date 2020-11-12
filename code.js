5ba834b876ab9cb0c1af59dd27e0db4d53f76737
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2020-11-12 14:19:05 -0600

    TDD testing..

--- code.js
+++ code.js
@@ -1,4 +1,9 @@
 // helloWorld function 
-function helloWorld() {
+const helloWorld = function() {
     return "Hello, World!";
+}
+
+// sayHello
+const sayHello = function() {
+    return "Hello";
 }
