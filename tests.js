730714011f38f73e4e1c577d09e1a5be7a89d3e1
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-04 17:52:37 -0600

    Exercise 1 completed

--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
+
     it('should be a defined function', function() {
       expect(typeof helloWorld).toBe('function');
     });
@@ -13,3 +14,20 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function() {
+    //Test for its a function
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    //Test for
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello()).toContain("Hello ");
+        });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+        });
+})
