e0ef810f0939db769bc46423744936d4e685fb0e
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-04 16:28:49 -0600

    Exercise 1 Personal test

--- code.js
+++ code.js
@@ -1,7 +1,41 @@
 // helloWorld function 
-function helloWorld() {
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+// function statement syntax
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+// function expression syntax (assigning an anonymous function to a variable)
+const helloWorld = function() {
     return "Hello, World!";
 }
+
 // I noticed that everything is not working now
 // We have to name it properly in the test.js file to hello();
 // Back to green when i changed the name back
+
+//--- Exercise #3
+// Does not work when we change the return string
+// message appears:
+//      -should return the string "Hello, World!" when executed
+
+//-- Exercise #4
+// When you insert a console log inside this function jasmine will only allow the first test to be done, which is
+// create a function.
+// with jasmine you have to be exact with what you want your functions and/or code to do.
+
+//-- Exercise #5
+// Syntax Same thing, error because with jasmine it makes sure your code is doind what it suppose to be doing.
+
+//-- Exercise #6
+// It does work.
+// works with a constant variable as well.
+
+
+//--- Exercise #1 Take your first "Test Drive" by writing your first test!
+
+function sayHello(insertName){
+    return "Hello "+insertName;
+}
