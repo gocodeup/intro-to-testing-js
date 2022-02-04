6a9279a61515e5de2640432a13f0c5c2a8915225
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-04 16:14:53 -0600

    Completed Exercise 2

--- code.js
+++ code.js
@@ -1,4 +1,7 @@
 // helloWorld function 
-function hello() {
+function helloWorld() {
     return "Hello, World!";
 }
+// I noticed that everything is not working now
+// We have to name it properly in the test.js file to hello();
+// Back to green when i changed the name back
