429a7d0433bc6ef3b34c5145ae24794e14577dc4
Derek Berkley <berkley.derek@gmail.com>
2022-03-22 11:24:45 -0500

    committing unit testing package and initial setup

--- code.js
+++ code.js
@@ -1,4 +1,10 @@
 // helloWorld function 
+
 function helloWorld() {
     return "Hello, World!";
 }
+
+//Function Expression works the same as defined function due to first class citizen category
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
