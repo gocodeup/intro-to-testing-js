1964e5d298f7253d43dcf2800d894e58c73fe4b3
Fernando Lopez <flopez00032@gmail.com>
2022-08-23 15:59:33 -0500

    step 6 finished

--- code.js
+++ code.js
@@ -3,6 +3,16 @@ const helloWorld = function () {
     return "Hello, World!";
 }
 
-function sayHello(string){
-    return "Hello, " +string + "!";
+// function sayHello(string){
+//     if (string === "Alex"){
+//         return "Hello, " + string + "!";
+//     }else if (string === "Pat"){
+//         return "Hello, " + string + "!";
+//     }else{
+//         return "Hello, Jane!";
+//     }
+// }
+
+function sayHello(input){
+    return "Hello, " + input + "!";
 }
--- tests.js
+++ tests.js
@@ -22,7 +22,21 @@ describe('sayHello', function () {
     it('should return a string', function (){
         expect(typeof sayHello()).toBe("string");
     });
-    it('should return the string "Hello, Jane!" when the string \"Jane"\ is passed', function() {
+    it('should return the string \"Hello, Jane!\"', function() {
         expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
+    it('should return the string "Hello, Jane!" when the string \"Jane"\ is passed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return "Hello, Pat!" when passed the string \"Pat\" is passed', function (){
+        expect(sayHello("Pat")).toBe("Hello, Pat!")
+    })
 });
+// Step 1: Add another (tiny) assertion! In tests.js, assert that sayHello("Pat") returns "Hello, Pat!".
+// Since our tests should be super simple, the assertion should be expect(sayHello("Pat")).toBe("Hello, Pat!")
+// Step 2: Run all tests and make sure that this newly added test is red and failing.
+//     Step 3: Again, you may feel the urge to jump to the "correct" answer already. Let's stay on target.
+//     Write just enough code to green the test. For this case, just enough code means adding another conditional
+//     such that if input === "Pat", then the function should have return "Hello, Pat!".
+// Step 4: Run all tests. Does each test turn green? If so, then we can proceed.
+//     Step 5: Refactor! It's definitely refactoring time!
